ee3bb0dedb4a42c68bd4d0d69eeea3be9c238ad9
Glenn <glenn.benz@gmail.com>
2021-04-15 14:25:53 -0400

    Trying something else

--- src/org/shared-library-test/Globals.groovy
+++
@@ -1,6 +0,0 @@
-package org.shared-library-test
-
-class Globals {
-   public String TAG = new Date.format("yyyyMMddHHmmss")
-   public boolean CONTINUE = true
-}
--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,28 +1,41 @@
-import org.shared-library-test.Globals
-
 def call(Map config=[:], Closure body) {
-   stage("Build") {
-      steps {
-         echo "TAG = ${TAG}"
-         echo "CONTINUE = ${CONTINUE}"
 
-         // Get some code from a GitHub repository
-         git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+   String TAG = new Date.format("yyyyMMddHHmmss")
+   boolean CONTINUE = true
 
-         // Run Maven on a Unix agent.
-         sh "mvn -Dmaven.test.failure.ignore=true clean package"
+   pipeline {
+      agent any
+   
+      tools {
+         // Install the Maven version configured as "M3" and add it to the path.
+         maven "M3"
       }
-
-      body()
-
-      post {
-         // If Maven was able to run the tests, even if some of the test
-         // failed, record the test results and archive the jar file.
-         success {
-            junit '**/target/surefire-reports/TEST-*.xml'
-            archiveArtifacts 'target/*.jar'
+   
+      stages {
+         stage("Build") {
+            steps {
+               echo "TAG = ${TAG}"
+               echo "CONTINUE = ${CONTINUE}"
+      
+               // Get some code from a GitHub repository
+               git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+      
+               // Run Maven on a Unix agent.
+               sh "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+      
+            body()
+      
+            post {
+               // If Maven was able to run the tests, even if some of the test
+               // failed, record the test results and archive the jar file.
+               success {
+                  junit '**/target/surefire-reports/TEST-*.xml'
+                  archiveArtifacts 'target/*.jar'
+               }
+            }
          }
       }
    }
-}
 
+}
