d3637b4fd29273c0acb89abbbbffc2e547a31f6d
Glenn <glenn.benz@gmail.com>
2021-04-15 10:54:29 -0400

    add post notification

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -15,5 +15,13 @@ def call(Map config=[:], Closure body) {
 
       body()
    }
+   post {
+      // If Maven was able to run the tests, even if some of the test
+      // failed, record the test results and archive the jar file.
+      success {
+         junit '**/target/surefire-reports/TEST-*.xml'
+         archiveArtifacts 'target/*.jar'
+      }
+   }
 }
 
